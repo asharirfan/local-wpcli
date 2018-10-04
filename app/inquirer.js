eb7aa03027f4310bad35a8dcdf28b52727b4cfc3
Ashar Irfan <mrasharirfan@gmail.com>
2018-10-04 23:35:50 +0500

    👌 IMPROVE: Input Questions

--- app/inquirer.js
+++ app/inquirer.js
@@ -8,27 +8,32 @@
 
 const inquirer = require('inquirer');
 
-const userInput = (name, input_type, message, error) => (
-	{
+const userInput = (name, input_type, message, error) => {
+	let question = {
 		name: name,
 		type: input_type,
-		message: message,
-		validate: function (value) {
+		message: message
+	};
+
+	if (name === 'remoteHostIP') {
+		question.default = '192.168.95.100';
+	} else if (name === 'remoteHostPort') {
+		question.validate = function (value) {
 			if (value.length) {
 				return true;
-			}
-			else {
+			} else {
 				return error;
 			}
-		}
+		};
 	}
-);
+	return question;
+};
 
 module.exports = {
 	askWebsiteDetails: () => {
 		const questions = [
-			userInput('remoteHostIP', 'input', 'Enter the IP address of the Remote Host:', 'Please enter the IP address of the Remote Host.'),
-			userInput('remoteHostPort', 'input', 'Enter the Port of the Remote Host:', 'Please enter the Port of the Remote Host.'),
+			userInput('remoteHostIP', 'input', 'Remote Host:', 'Please enter the IP address of the Remote Host.'),
+			userInput('remoteHostPort', 'input', 'Remote Port:', 'Please enter the Port of the Remote Host.'),
 		];
 		return inquirer.prompt(questions);
 	}
