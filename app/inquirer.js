0da49d2a220ebcc52bd2a9d0bc33d4c808e35fdc
Ashar Irfan <mrasharirfan@gmail.com>
2018-10-04 23:44:10 +0500

    👌 IMPROVE: Code Docs

--- app/inquirer.js
+++ app/inquirer.js
@@ -6,9 +6,20 @@
 
 'use strict';
 
-const inquirer = require('inquirer');
+const inquirer = require('inquirer'); // Include inquirer.
 
+/**
+ * Generate Question Object
+ *
+ * Generate input question object for inquirer.
+ *
+ * @param {string} name       – Input name.
+ * @param {string} input_type – Input type.
+ * @param {string} message    – Input prompt.
+ * @param {string} error      – Input error message.
+ */
 const userInput = (name, input_type, message, error) => {
+	// Inquirer question object.
 	let question = {
 		name: name,
 		type: input_type,
@@ -16,8 +27,10 @@ const userInput = (name, input_type, message, error) => {
 	};
 
 	if (name === 'remoteHostIP') {
+		// Set default IP if the question is for Remote Host.
 		question.default = '192.168.95.100';
 	} else if (name === 'remoteHostPort') {
+		// Set error message if the question is for Remote IP.
 		question.validate = function (value) {
 			if (value.length) {
 				return true;
@@ -26,14 +39,19 @@ const userInput = (name, input_type, message, error) => {
 			}
 		};
 	}
+
+	// Return the question object.
 	return question;
 };
 
 module.exports = {
+	/**
+	 * Prompt Input Questions.
+	 */
 	askWebsiteDetails: () => {
 		const questions = [
-			userInput('remoteHostIP', 'input', 'Remote Host:', 'Please enter the IP address of the Remote Host.'),
-			userInput('remoteHostPort', 'input', 'Remote Port:', 'Please enter the Port of the Remote Host.'),
+			userInput('remoteHostIP', 'input', 'Remote Host:', 'Please enter the IP address of the Remote Host.'), // Remote Host.
+			userInput('remoteHostPort', 'input', 'Remote Port:', 'Please enter the Port of the Remote Host.'), // Remote IP.
 		];
 		return inquirer.prompt(questions);
 	}
