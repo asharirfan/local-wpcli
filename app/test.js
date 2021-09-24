7ea1ce80cbe792c57200be167cf2c15936afd999
Ashar Irfan <mrasharirfan@gmail.com>
2021-09-24 22:43:34 +0500

    Improve test command error handling

--- app/test.js
+++ app/test.js
@@ -6,16 +6,33 @@
 
 'use strict';
 
-const { exec } = require('child_process');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const chalk = require('chalk');
 
 module.exports = async () => {
-	await exec('wp option get siteurl', (err, stdout, stderr) => {
-		if (err) {
-			console.log(`\n❌ ${stderr}`);
-			return;
-		}
 
-		console.log(chalk.green(`\n✅  Successfully connected to ${stdout}`));
+	const testResult = await exec(
+		'wp option get siteurl'
+	).catch(({ stderr }) => {
+
+		// Test command failed due to some reason.
+		console.error(`\n❌ ${stderr}`); // eslint-disable-line no-console
+		return false;
 	});
+
+	if (false === testResult) {
+		return;
+	}
+
+	const { stdout, stderr } = testResult;
+
+	if (stderr) {
+		console.error(`\n❌ ${stderr}`); // eslint-disable-line no-console
+		return;
+	}
+
+	console.log( // eslint-disable-line no-console
+		chalk.green(`\n✅  Successfully connected to ${stdout}`)
+	);
 };
